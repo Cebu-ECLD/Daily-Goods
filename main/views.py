868e572f8f84fc29dba63349593290fd6a056160
村瀬大心 <murasetaishin@murasedaishinnoMacBook-Pro.local>
2019-09-06 12:48:52 +0800

    json返却昨日

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from flask import request, redirect, url_for, render_template, flash
 from main import app
+import requests
+import pandas as pd
 #from flaskr.models import Entry
 
 @app.route('/')
@@ -13,9 +15,23 @@ def index():
 def result():
     title = "resut"
     message = "resultに移動したよ"
-    
+    word = request.args.get('search')
+    item_url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706'
+    item_parameters = {
+                 'applicationId': 1034112430128448197,
+                'shopcode' : 'rf222101-fuji',
+                'format': 'json',
+                'formatVersion': 2,
+                'genreId': 0,
+                'keyword': word,
+                'shopName': '楽天24',
+                 'hits': 5,
+    }
+    r = requests.get(item_url, params=item_parameters)
+    item_data = r.json()
+
     return render_template('result.html',
-                    message=message, title=title)
+                    message=message, title=title, word = word, item_data=item_data)
 
 @app.route('/page', methods=['POST', 'GET'])
 def page():
