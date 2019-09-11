a6f7294ba92a98fe72f781fa4236df30f197832c
村瀬大心 <murasetaishin@murasedaishinnoMacBook-Pro.local>
2019-09-11 09:41:01 +0800

    connection flask, htmll and db

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,33 @@
 from flask import Flask
-#from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config.from_object('main.config')
+app.config.from_object('main.config')
 
-# db = SQLAlchemy(app)
+# from sqlalchemy import *
+# from sqlalchemy.orm import *
+# from sqlalchemy.ext.declarative import declarative_base
+
+# dialect = "mysql"
+# driver = "mysqldb"
+# username = "root"
+# password = ""
+# host = "127.0.0.1"
+# port = "3306"
+# database = "rakuten_db"
+# charset_type = "utf8"
+# db_url = f"{dialect}+{driver}://{username}:{password}@{host}:{port}/{database}?charset={charset_type}"
+
+# # DB接続するためのEngineインスタンス
+# ENGINE = create_engine(db_url, echo=True)
+
+import pymysql.cursors
+
+conn = pymysql.connect(host='127.0.0.1',
+                    user='root',
+                    db='rakuten_db',
+                    charset='utf8mb4',
+                    cursorclass=pymysql.cursors.DictCursor)
+
 
 import main.views
--- main/views.py
+++ main/views.py
@@ -1,20 +1,28 @@
 from flask import request, redirect, url_for, render_template, flash
 from main import app
 import requests
-import pandas as pd
+import main.__init__ as init
 #from flaskr.models import Entry
 
 @app.route('/')
 def index():
     title = "ようこそ"
     message = "楽天へ"
+
+    # with init.conn.cursor() as cursor:
+    #     sql = "SELECT * FROM product_table"
+    #     cursor.execute(sql)
+    #     result = cursor.fetchall()
+        
+
     return render_template('index.html',
-                           message=message, title=title)
+                           message=message, title=title, result=result)
 
 @app.route('/result', methods=['POST', 'GET'])
 def result():
     title = "resut"
     message = "resultに移動したよ"
+    # API使う場合
     word = request.args.get('search')
     item_url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706'
     item_parameters = {
@@ -30,14 +38,22 @@ def result():
     r = requests.get(item_url, params=item_parameters)
     item_data = r.json()
 
+    #db使う場合(固定してるからここ変更する)
+    word = request.args.get('search')
+    with init.conn.cursor() as cursor:
+        sql = "SELECT * FROM product_table"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
     return render_template('result.html',
-                    message=message, title=title, word = word, item_data=item_data)
+                    message=message, title=title, word = word, item_data=item_data, result=result)
 
 @app.route('/page', methods=['POST', 'GET'])
 def page():
     title = "page"
     message = "pageに移動したよ"
-    
+    good_info = request.args.get('good_info')
+
     return render_template('page.html',
-                    message=message, title=title)
+                    message=message, title=title, good_info=good_info)
 
