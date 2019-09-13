0d9317c51519eabba569f1173444222b06500fb2
村瀬大心 <murasetaishin@murasedaishinnoMacBook-Pro.local>
2019-09-13 09:39:30 +0800

    commit

--- main/views.py
+++ main/views.py
@@ -2,43 +2,16 @@ from flask import request, redirect, url_for, render_template, flash
 from main import app
 import requests
 import main.__init__ as init
-#from flaskr.models import Entry
+
 
 @app.route('/')
 def index():
-    title = "ようこそ" 
-    message = "楽天へ"
-
-    # with init.conn.cursor() as cursor:
-    #     sql = "SELECT * FROM product_table"
-    #     cursor.execute(sql)
-    #     result = cursor.fetchall()
         
-
-    return render_template('index.html',
-                           message=message, title=title, result=result)
+    return render_template('index.html')
 
 @app.route('/result', methods=['POST', 'GET'])
 def result():
-    title = "resut"
-    message = "resultに移動したよ"
-    # API使う場合
-    # word = request.args.get('search')
-    # item_url = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706'
-    # item_parameters = {
-    #              'applicationId': 1034112430128448197,
-    #             'shopcode' : 'rf222101-fuji',
-    #             'format': 'json',
-    #             'formatVersion': 2,
-    #             'genreId': 0,
-    #             'keyword': word,
-    #             'shopName': '楽天24',
-    #              'hits': 5,
-    # }
-    # r = requests.get(item_url, params=item_parameters)
-    # item_data = r.json()
-
-    #db使う場合(固定してるからここ変更する)
+  
     word = request.args.get('search')
     with init.conn.cursor() as cursor:
         sql = "SELECT * FROM product_table WHERE itemName LIKE " + "'%" + word + "%'" + " OR itemCaption LIKE '%" + word + "%'"
@@ -48,13 +21,11 @@ def result():
     if(len(result)==0):
         result = ['nothing']
 
-    return render_template('result.html',
-                    message=message, title=title, word = word, result=result)
+    return render_template('result.html', word = word, result=result)
 
 @app.route('/page', methods=['POST', 'GET'])
 def page():
-    title = "page"
-    message = "pageに移動したよ"
+
     good_info = request.args.get('good_info')
 
     with init.conn.cursor() as cursor:
@@ -62,7 +33,6 @@ def page():
         cursor.execute(sql)
         result = cursor.fetchall()
 
-    return render_template('page.html',
-                    message=message, title=title, good_tag=good_info, good_info=result)
+    return render_template('page.html', good_tag=good_info, good_info=result)
 
 
--- manage.py
+++ manage.py
@@ -2,31 +2,3 @@ from main import app
 
 app.run(host='127.0.0.1', port=5000, debug=True)
 
-
-# # Routing
-# @app.route('/')
-# def index():
-#     title = "ようこそ"
-#     message = "楽天へ"
-#     return render_template('./main/templates/index.html',
-#                            message=message, title=title)
-
-# @app.route('/result', methods=['POST', 'GET'])
-# def post():
-#     title = "resut"
-#     message = "resultに移動したよ"
-    
-#     return render_template('templates/result.html',
-#                     message=message, title=title)
-
-# @app.route('/page', methods=['POST'])
-# def functions():
-#     title = "page"
-#     message = "pageに移動"
-
-#     return render_template('.main/templates/page.html',
-#                             message=message, title=title)
-
-# if __name__ == '__main__':
-#     app.debug = True
-#     app.run(host='0.0.0.0', port=3000)
